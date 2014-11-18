ba4f04d1a6f879e87872e9e5f03db1f4bb048cf8
amyvs <amydecat@yahoo.com>
2014-11-17 17:04:16 -0800

    add 'add', 'reset', and 'all' listeners in js/views/app.js file

--- js/views/app.js
+++ js/views/app.js
@@ -1,7 +1,7 @@
 var app = app || {};
 
 	app.AppView = Backbone.View.extend({
-
+		collection: app.messages,
 		el: '#app',
 
 		events: {
@@ -10,16 +10,28 @@ var app = app || {};
 
 		initialize: function() {
 			this.$input = this.$('#new-message');
-			this.listenTo(app.Messages, "add", this.addMessage);
+			this.listenTo(app.Messages, "add", this.addOne);
+			this.listenTo(app.Messages, "reset", this.addAll);
+			this.listenTo(app.Messages, "all", this.render)
 		},
 
 		createMessage: function() {
-			alert('clicked');
+			var message = new app.Message();
 			if (!this.$input.val().trim()) {
 				return;
 			}
-			app.Message.create({ body: this.$input.val().trim() });
+			message.create({ body: this.$input.val().trim() });
 			this.$input.val('');
+		},
+
+		addOne: function(message){
+			var view = new app.MessageView({model: message});
+			$('#chat-list').append(view.render().el);
+		},
+
+		addAll: function(){
+			$('#chat-list').html('');
+			app.Messages.each(this.addOne, this);
 		}
 
 	});
